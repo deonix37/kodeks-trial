fc58c8cc34ed689f7c95937bb8e6da4e7bec387b
deo <dasdasgon@gmail.com>
2023-03-11 23:39:08 +0300

    Update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\NewsCategory;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $users = User::factory(100)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        NewsCategory::factory(10)->create();
+        NewsCategory::each(function ($category) use ($users) {
+            $category->subscriptions()->createMany(
+                $users->random(rand(0, 100))->map(fn ($user) => [
+                    'user_email' => $user->email,
+                    'user_name' => $user->name,
+                    'unsubscription_key' => Str::random(),
+                ]),
+            );
+        });
     }
 }
